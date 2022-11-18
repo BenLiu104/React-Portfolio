45ee4a5787d5cd5a495bdf117a2a9d98320cb213
BenLiu <liumanpan1990@hotmail.com>
2022-11-17 22:56:38 -0500

    personal information update

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,5 +1,26 @@
 import React from 'react';
-
+import avatar from '../../images/avatar.jpeg';
 export default function About() {
-  return <div>About Page</div>;
+  return (
+    <div className="px-2">
+      <h2 className="text-center">About Me</h2>
+      <img
+        src={avatar}
+        alt="profile_picture"
+        className="rounded-circle text-center"
+        width="30%"
+      />
+      <section>
+        <p>
+          I am a new full stack web developer graduated from University of
+          Toronto School. After 12-week intensive bootcamp training, I learnt so
+          many useful concept and skills on web development. On the other hand,
+          because of my previous IT jobs experience, I can pick up the
+          programming concept and skill quickly. Also it makes me easier to
+          understand the front-end and back-end relationship. I have well
+          prepared myself get into the web developing journey.
+        </p>
+      </section>
+    </div>
+  );
 }
--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,5 +1,38 @@
 import React from 'react';
+import CV from '../../files/CV with WEB (LIU MAN PAN) 2022 - CA .pdf';
 
 export default function Resume() {
-  return <div>Resume Page</div>;
+  return (
+    <div>
+      <div>
+        Download my{' '}
+        <a href={CV} download>
+          Resume
+        </a>
+      </div>
+      <section>
+        <h3>Front-end proficiencies</h3>
+        <ul>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>Bootstrap</li>
+          <li>React.js</li>
+          <li>JQuery</li>
+          <li>Responsive design</li>
+        </ul>
+      </section>
+
+      <section>
+        <h3>Back-end proficiencies</h3>
+        <ul>
+          <li>REST</li>
+          <li>APIs</li>
+          <li>Express.js</li>
+          <li>MySQL & Sequelize</li>
+          <li>MongoDB & Mongoose</li>
+          <li>GraphSQL</li>
+        </ul>
+      </section>
+    </div>
+  );
 }
