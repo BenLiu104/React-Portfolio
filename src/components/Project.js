04aeb0aa5c01efc62faa103b056f791cd5148ebc
BenLiu <liumanpan1990@hotmail.com>
2022-11-17 22:55:45 -0500

    add project content to portfolio page

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,11 +1,35 @@
 import React from 'react';
+import githubIcon from '../images/github-sign.png';
 
-export default function Project({ title, url, image }) {
+export default function Project({ title, url, image, Repo }) {
+  const style = {
+    width: '100%',
+    height: '20rem',
+    objectFit: 'cover',
+  };
+
   return (
-    <div className="card">
-      <div className="card-header">{title}</div>
-      <div className="card-body">{url}</div>
-      <div className="card-body">{image}</div>
+    <div className="col p-4">
+      <div className="card">
+        <img
+          src={image}
+          className="card-img-top border"
+          alt={title}
+          style={style}
+        />
+        <div className="card-body">
+          <h4 className="card-title">{title}</h4>
+          {/* <p className="card-text">Go to Application</p> */}
+          <div className="text-end">
+            <a href={url} className="card-text">
+              <span>Go to Application</span>
+            </a>
+            <a href={Repo} className="card-text mx-3">
+              <img src={githubIcon} alt="" width="20px"></img>
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,18 +1,63 @@
 import React from 'react';
 import Project from '../Project';
+
+import project1 from '../../images/project1.png';
+import project2 from '../../images/project2.png';
+
 export default function Portfolio() {
   const projects = [
-    { title: 'project1', url: 'http://url1.com', image: 'image1.png' },
-    { title: 'project2', url: 'http://url2.com', image: 'image2.png' },
+    {
+      title: 'Flight Searching',
+      url: 'https://benliu104.github.io/Project01-FlightPlanner/',
+      image: project1,
+      Repo: 'https://github.com/BenLiu104/Project01-FlightPlanner',
+    },
+    {
+      title: 'Online Store',
+      url: 'https://your-store-jibstudios.herokuapp.com/',
+      image: project2,
+      Repo: 'https://github.com/Iva416/yourStore',
+    },
+    {
+      title: 'Code Quiz',
+      url: 'https://benliu104.github.io/Code-Quiz/',
+      image:
+        'https://github.com/BenLiu104/Code-Quiz/blob/main/assets/images/Step1.png?raw=true',
+      Repo: 'https://github.com/BenLiu104/Code-Quiz',
+    },
+    {
+      title: 'Note Taker',
+      url: 'https://as1-note-taker.herokuapp.com/',
+      image:
+        'https://github.com/BenLiu104/NoteTaker/blob/main/image/step1.png?raw=true',
+      Repo: 'https://github.com/BenLiu104/NoteTaker',
+    },
+    {
+      title: 'Weather Dashboard',
+      url: 'https://benliu104.github.io/Weather-Dashboard/',
+      image:
+        'https://github.com/BenLiu104/Weather-Dashboard/blob/main/assets/images/step1.png?raw=true',
+      Repo: 'https://github.com/BenLiu104/Weather-Dashboard',
+    },
+    {
+      title: 'Workday Scheduler',
+      url: 'https://benliu104.github.io/Work-Day-Scheduler/',
+      image:
+        'https://github.com/BenLiu104/Work-Day-Scheduler/blob/main/Assets/step1.png?raw=true',
+      Repo: 'https://github.com/BenLiu104/Work-Day-Scheduler',
+    },
   ];
+
   return (
-    <div>
-      {projects.map((project) => {
+    <div className="row row-cols-1 row-cols-lg-2 g-2">
+      {projects.map((project, index) => {
         return (
           <Project
+            key={index}
             title={project.title}
             url={project.url}
             image={project.image}
+            Repo={project.Repo}
           />
         );
       })}
