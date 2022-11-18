fbdc796a34387b24015b4a151e96fc80a6b79b24
BenLiu <liumanpan1990@hotmail.com>
2022-11-17 22:55:14 -0500

    complete header and footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,30 @@
 import React, { useState } from 'react';
+import githubIcon from '../images/github-sign.png';
+import linkedinIcon from '../images/001-linkedin.png';
+import twitterIcon from '../images/twitter.svg';
 
 export default function Footer(props) {
   return (
-    <footer className="bg-light text-center text-lg-start fixed-bottom">
+    <footer className="bg-light text-center  sticky-bottom">
+      <div>
+        <a href="https://github.com/BenLiu104" className="mx-1">
+          <img src={githubIcon} width="25px" alt="github"></img>
+        </a>
+        <a
+          href="https://www.linkedin.com/in/ben-liu-543487188/"
+          className="mx-1"
+        >
+          <img src={linkedinIcon} width="25px" alt="linkedin"></img>
+        </a>
+        <a href="https://twitter.com/liumanpan" className="mx-1">
+          <img src={twitterIcon} alt="Bootstrap" width="25" />
+        </a>
+      </div>
       <div
         className="text-center p-3"
         style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}
       >
         © 2022 Copyright:
-        <a className="text-dark" href="https://github.com/BenLiu104">
-          Ben Liu
-        </a>
       </div>
     </footer>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,12 @@
 import React, { useState } from 'react';
-
+import headerImg from '../images/headerImg.jpg';
 export default function Header(props) {
-  return <div>{props.children}</div>;
+  return (
+    <div
+      className="text-bg-dark sticky-top"
+      style={{ backgroundImage: `url(${headerImg})` }}
+    >
+      <div>{props.children}</div>
+    </div>
+  );
 }
