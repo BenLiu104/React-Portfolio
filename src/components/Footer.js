34df4457884354abc3e5e1b5b76a054a1369689c
BenLiu <liumanpan1990@hotmail.com>
2023-01-13 12:23:33 -0800

    add email icon

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,11 +2,15 @@ import React, { useState } from 'react';
 import githubIcon from '../images/github-sign.png';
 import linkedinIcon from '../images/001-linkedin.png';
 import twitterIcon from '../images/twitter.svg';
+import emailIcon from '../images/email-icon.png';
 
 export default function Footer(props) {
   return (
     <footer className="bg-light text-center  sticky-bottom">
       <div>
+        <a href="mailto:liumanben1990@gmail.com" className="mx-1">
+          <img src={emailIcon} width="25px" alt="github"></img>
+        </a>
         <a href="https://github.com/BenLiu104" className="mx-1">
           <img src={githubIcon} width="25px" alt="github"></img>
         </a>
